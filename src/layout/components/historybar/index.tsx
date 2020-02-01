b4c8517479dc8d65343dff5f3320722fad747aaf
lilindog <lilin@lilin.site>
2020-02-01 14:02:48 +0800

    完成history滚动

--- src/layout/components/historybar/index.tsx
+++ src/layout/components/historybar/index.tsx
@@ -20,6 +20,7 @@ export default class HistoryBar extends React.Component {
 
     init () {
         this.initUseStore();
+        this.initMouseWheel();
     }
 
     initUseStore () {
@@ -31,6 +32,56 @@ export default class HistoryBar extends React.Component {
             this.state.asideMenusFold = layout.asideMenus.fold;
             this.setState({});
         });
+    }
+
+    initMouseWheel () {
+        const $out: any = this.refs["out"], $in: any = this.refs["in"];
+        $out.addEventListener("mousewheel", ({ wheelDelta }): void => {
+            if (wheelDelta > 0) action.call(this, 1);
+            if (wheelDelta < 0) action.call(this, -1);
+        });
+        $out.addEventListener("DOMMouseScroll", ({ detail }): void => {
+            if (detail < 0) action.call(this, 1);
+            if (detail > 0) action.call(this, -1);
+        });
+        //state大于0滚轮上滚动，小于0滚轮下滚动
+        function action(state: number): void {
+            const step = 20;
+            if ($in.offsetWidth <= $out.offsetWidth) return;
+            if (state > 0) {
+                console.log("上滚动");
+                const oldLeft: number = parseFloat(getComputedStyle($in).left);
+                let offset: number = step;
+                if (oldLeft >= 0) return;
+                if (oldLeft + offset > 0)  {
+                    offset = Math.abs(oldLeft);
+                } else {
+                    offset = step;
+                }
+                $in.style.left = (oldLeft + offset) + "px";
+            }
+            if (state < 0) {
+                console.log("下滚动");
+                const 
+                scrollRange: number = $in.offsetWidth - $out.offsetWidth,
+                oldLeft: number = parseFloat(getComputedStyle($in).left);
+                let offset: number = step;
+                if (Math.abs(oldLeft) >= scrollRange) return;
+                if (Math.abs(oldLeft) + offset > scrollRange) {
+                    offset = scrollRange - Math.abs(oldLeft);
+                    console.log("jjj");
+                    console.log(offset);
+                } else {
+                    offset = step;
+                }
+                $in.style.left = (oldLeft - offset) + "px";
+            }
+        }
+    }
+
+    //获取焦点
+    focus (): void {
+        
     }
 
     //侧边菜单折叠按钮
@@ -55,8 +106,8 @@ export default class HistoryBar extends React.Component {
                 <div className="end-wrap">
                     <Button type="primary" onClick={this.asideSetBtn.bind(this)} shape="circle" icon={state.asideMenusFold ? "menu-unfold" : "menu-fold"}/>
                 </div>
-                <div className="history">
-                    <div className="child-inwrap">
+                <div className="history" ref="out">
+                    <div className="child-inwrap" ref="in">
                     {state.childs.map(item => {
                         return (
                             <div className="child-btn">
