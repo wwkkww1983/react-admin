8d480681cdb88d403725cee65c653c2e67df1391
lilindog <lilin@lilin.site>
2020-11-25 22:52:13 +0800

    增加设置-> 是否启用后付费

--- src/pages/pileSetting/index.tsx
+++ src/pages/pileSetting/index.tsx
@@ -4,7 +4,8 @@ import {
     Form,
     Input,
     Button,
-    message
+    message,
+    Radio
 } from "antd";
 import { input, initLife } from "../../utils/utils";
 import { saveSetting, getSetting } from "../../api/setting";
@@ -16,6 +17,7 @@ export default class PileSetting extends React.Component {
         saveLoading: false,
         data: {
             charging_station_min_balance: 0, //充电桩最高欠款额度（单位分）
+            post_paid: 1,
         }
     }
 
@@ -39,16 +41,17 @@ export default class PileSetting extends React.Component {
         }
         NProgress.done();
         const data = {
-            charging_station_min_balance: res.data.charging_station_min_balance / 100
+            charging_station_min_balance: res.data.charging_station_min_balance / 100,
+            post_paid: Number(res.data.post_paid)
         }
-        console.log(data);
         this.setState({data});
     }
 
     //保存配置
     async save () {
         const data = {
-            charging_station_min_balance: this.state.data.charging_station_min_balance * 100
+            charging_station_min_balance: this.state.data.charging_station_min_balance * 100,
+            post_paid: this.state.data. post_paid
         }
         this.setState({saveLoading: true});
         try {
@@ -74,6 +77,12 @@ export default class PileSetting extends React.Component {
                         className="long-input"
                         addonAfter="元" 
                         />
+                    </Form.Item>
+                    <Form.Item label="是否启用后付费">
+                        <Radio.Group onChange={input.bind(this, "data.post_paid")} value={state.data.post_paid}>
+                            <Radio value={1}>是</Radio>
+                            <Radio value={0}>否</Radio>
+                        </Radio.Group>
                     </Form.Item>
                     <Form.Item className="Item">
                         <Button icon="cloud-upload" onClick={this.save.bind(this)} loading={state.saveLoading}>保存</Button>
